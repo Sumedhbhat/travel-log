624e66efefe59b806353cd5f0097d9a482a743cd
Sumedh S Bhat <sumedhbhat01@gmail.com>
2025-07-02 12:53:58 +0530

    Added Basic Landing Page

--- app.vue
+++ app.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <button class="btn btn-primary">
-      Secondary
-    </button>
-  </div>
+  <NuxtLayout>
+    <div class="hero bg-base-200 container mx-auto mt-4 p-4">
+      <div class="hero-content text-center min-h-96">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">
+            Travel Log
+          </h1>
+          <p class="py-6">
+            Keep Track of your travels with Travel Log. Document your adventures, mark destinations on maps, upload photos from memorable moments, and create beautiful journals of your journeys. Whether you're a globetrotter or weekend explorer, Travel Log helps you preserve those special memories. Sign in to get started.
+          </p>
+          <button class="btn btn-accent">
+            Sign In
+            <Icon name="tabler:brand-github" size="24" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </NuxtLayout>
 </template>
