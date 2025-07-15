ced840ffabaca669717ece430adaeb4106cf6db8
Sumedh S Bhat <sumedhbhat01@gmail.com>
2025-07-15 21:09:50 +0530

    Added auth button

---
+++ components/auth-button.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+const loading = ref(false);
+
+async function signIn() {
+  loading.value = true;
+  await authClient.signIn.social({
+    provider: "github",
+  });
+  loading.value = false;
+}
+</script>
+
+<template>
+  <button :disabled="loading" class="btn btn-accent" @click="signIn">
+    Sign In
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,16 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "./db/index.ts";
+import env from "./env.ts";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
