58e64a2e76859b8a22aa600695738a1a4848f6a0
Sumedh S Bhat <sumedhbhat01@gmail.com>
2025-08-18 21:42:53 +0530

    feat(app): Added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import "./lib/env";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt"],
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   eslint: {
