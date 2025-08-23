e1fe8b5c0c20f8f2aaef665772f8cc31170732a6
Sumedh Bhat <sumedhbhat01@gmail.com>
2025-08-23 18:26:54 +0530

    Integrating Github Auth (#55)
    * chore(app): Changed name of the build pipeline
    
    * Added auth button
    
    * feat(app): Added pinia
    
    * fix(app): Fixed issue with dashboard page
    
    * fix(app): Repaired conflicting env name
    
    * Added variables to github pipeline

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,16 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "./db/index.ts";
+import env from "./env.ts";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  socialProviders: {
+    github: {
+      clientId: env.GH_CLIENT_ID,
+      clientSecret: env.GH_CLIENT_SECRET,
+    },
+  },
 });
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <div class="container mx-auto mt-4 p-4">
+      <h1 class="text-3xl font-bold mb-4">
+        Dashboard
+      </h1>
+      <p class="text-lg">
+        Welcome to your Travel Log dashboard! You have successfully signed in.
+      </p>
+    </div>
+  </NuxtLayout>
+</template>
